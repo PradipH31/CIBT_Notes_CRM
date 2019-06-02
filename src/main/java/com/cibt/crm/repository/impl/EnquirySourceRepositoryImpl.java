b9366f3f52024d2667b06a78c0959b4e834bc472
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-02 18:37:36 +0545

    Declared repository & jdbctemplate with annotation Executed queries

--- src/main/java/com/cibt/crm/repository/impl/EnquirySourceRepositoryImpl.java
+++ src/main/java/com/cibt/crm/repository/impl/EnquirySourceRepositoryImpl.java
@@ -8,16 +8,27 @@ package com.cibt.crm.repository.impl;
 import com.cibt.crm.entity.master.EnquirySource;
 import com.cibt.crm.repository.EnquirySourceRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author HP B&O
  */
+@Repository
 public class EnquirySourceRepositoryImpl implements EnquirySourceRepository {
+
+    @Autowired
+    private JdbcTemplate template;
 
     @Override
     public int insert(EnquirySource model) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "insert into mst_enquiry_sources(source_name,source_color)"
+                + " values(?,?)";
+        return template.update(sql, new Object[]{
+            model.getName(), model.getColor()
+        });
     }
 
     @Override
