e5535550211457b4ae5a95052d5f96b9f72877ac
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-02 17:51:30 +0545

    Set jdbctemplate bean from appconfiguration along with datasource

--- src/main/java/com/cibt/crm/configure/AppConfiguration.java
+++ src/main/java/com/cibt/crm/configure/AppConfiguration.java
@@ -7,6 +7,8 @@ package com.cibt.crm.configure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -20,5 +22,20 @@ public class AppConfiguration {
     @Bean
     public ViewResolver getViewResolver() {
         return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+    }
+
+    @Bean
+    public DriverManagerDataSource getDataSource() {
+        DriverManagerDataSource source = new DriverManagerDataSource();
+        source.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        source.setUrl("jdbc:mysql://localhost/crm");
+        source.setUsername("root");
+        source.setPassword("");
+        return source;
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
     }
 }
