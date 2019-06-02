9eac0b629eebf448fdfa928b989963ab80968f98
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-03 05:38:07 +0545

    Configured controller for index(without data)

--- src/main/java/com/cibt/crm/controller/master/EnquirySourceController.java
+++ src/main/java/com/cibt/crm/controller/master/EnquirySourceController.java
@@ -6,41 +6,52 @@
 package com.cibt.crm.controller.master;
 
 import com.cibt.crm.dto.EnquirySourceDTO;
+import com.cibt.crm.service.EnquirySourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
  * @author HP B&O
  */
+@Controller
+@RequestMapping(value = "/admin/master/enquiry/sources")
 public class EnquirySourceController extends CRUDController<EnquirySourceDTO> {
-    
+
+    @Autowired
+    private EnquirySourceService service;
+
+    @GetMapping
     @Override
     public String index() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "master/enquirysource/index";
     }
-    
+
     @Override
     public String add() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public String edit(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public String save(EnquirySourceDTO model) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public String detail(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public String delete(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
