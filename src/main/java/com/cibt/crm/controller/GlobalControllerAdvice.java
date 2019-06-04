27d7163236330f82203cd01537111f52f634af00
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-05 02:07:18 +0545

    Used globalAdvice with database

--- src/main/java/com/cibt/crm/controller/GlobalControllerAdvice.java
+++ src/main/java/com/cibt/crm/controller/GlobalControllerAdvice.java
@@ -5,6 +5,8 @@
  */
 package com.cibt.crm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,8 +18,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
+    @Autowired
+    private JdbcTemplate template;
+
     @ModelAttribute
     public void getProjectName(Model model) {
         model.addAttribute("projectName", "CIBT CRM Project");
+    }
+
+    @ModelAttribute
+    public void getTotalEnquiries(Model model) {
+        String sql = "select count(1) from tbl_enquiries where is_read=0";
+        int total = template.queryForObject(sql, Integer.class);
+        model.addAttribute("totalEnquiries", total);
     }
 }
