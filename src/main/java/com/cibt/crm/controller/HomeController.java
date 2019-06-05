9ab5acf0adfd0bfd050fb43601c5bbb3c6147e2d
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-05 23:26:50 +0545

    Used hibernate for insert or update

--- src/main/java/com/cibt/crm/controller/HomeController.java
+++ src/main/java/com/cibt/crm/controller/HomeController.java
@@ -5,9 +5,15 @@
  */
 package com.cibt.crm.controller;
 
+import com.cibt.crm.entity.Campaign;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -16,9 +22,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/")
 public class HomeController {
-
+    
+    @Autowired
+    private SessionFactory sessionFactory;
+    private Session session;
+    private Transaction transaction;
+    
     @GetMapping
+    @ResponseBody
     public String index() {
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+//        session.saveOrUpdate(new Campaign());
+        session.save(new Campaign());
+        transaction.commit();
         return "index";
     }
 }
